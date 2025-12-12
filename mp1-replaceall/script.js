df059527e5a990a7c441ffe82c411ab18aab5644
Shelley Magdalan <sMagdalan@mcm.edu.ph>
2025-12-12 23:27:15 +0800

    Update script.js

--- mp1-replaceall/script.js
+++ mp1-replaceall/script.js
@@ -1,14 +1,13 @@
 let txtInput = document.querySelector('#txtInput');
 let btnReplaceAll = document.querySelector('#btnReplaceAll');
 let txtOutput = document.querySelector('#txtOutput');
-
+let resultValue = txtOutput.querySelector('.result-value');
 
 btnReplaceAll.addEventListener('click', function () {
-
-
     let str = txtInput.value;
 
-    let result = str.replaceAll(" ", "");
+    let trimmed = str.trim();
+    let result = trimmed.replaceAll(" ", "");
 
-    txtOutput.textContent = result;
+    resultValue.textContent = result;
 });
