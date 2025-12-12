19584f1e70b7458c53caffe49e9050324954eeaf
Shelley Magdalan <sMagdalan@mcm.edu.ph>
2025-12-12 23:35:52 +0800

    Update script.js

--- mp4-countcharacters/script.js
+++ mp4-countcharacters/script.js
@@ -1,14 +1,14 @@
 let txtInput = document.querySelector('#txtInput');
 let btnCount = document.querySelector('#btnCount');
 let txtOutput = document.querySelector('#txtOutput');
+let resultValue = txtOutput.querySelector('.result-value');
 
 btnCount.addEventListener('click', function () {
-
     let str = txtInput.value;
 
     let trimmed = str.trim();
 
     let count = trimmed.length;
 
-    txtOutput.textContent = count;
+    resultValue.textContent = count;
 });
