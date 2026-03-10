672db73ca87770fc75806678fedc5f9197c5603a
Shelley Magdalan <sMagdalan@mcm.edu.ph>
2026-03-10 20:19:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ const themeBtn = document.getElementById('themeBtn');
 const body = document.body;
 
 window.addEventListener('DOMContentLoaded', () => {
+
     const savedTheme = localStorage.getItem('theme');
+
     if (savedTheme === 'dark') {
         body.classList.add('dark-mode');
         themeBtn.textContent = '☀️ Light Mode';
     }
+
 });
 
 themeBtn.addEventListener('click', () => {
+
     body.classList.toggle('dark-mode');
 
     if (body.classList.contains('dark-mode')) {
+
         themeBtn.textContent = '☀️ Light Mode';
         localStorage.setItem('theme', 'dark');
+
     } else {
+
         themeBtn.textContent = '🌙 Dark Mode';
         localStorage.setItem('theme', 'light');
+
     }
+
 });
